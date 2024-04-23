3ebc1efefef21a270a06cb56f59a89feb3350353
Игорь <tigor7750@gmail.com>
2024-04-23 12:09:43 +0300

    test

--- src/main/kotlin/ru/itech/sno_api/dto/LectureDTO.kt
+++ src/main/kotlin/ru/itech/sno_api/dto/LectureDTO.kt
@@ -1,53 +1,41 @@
 package ru.itech.sno_api.dto
 
 import io.swagger.v3.oas.annotations.media.Schema
-import jakarta.transaction.Transactional
 import ru.itech.sno_api.entity.LectureEntity
-import ru.itech.sno_api.repository.CourseRepository
+import ru.itech.sno_api.entity.toDTO
+import ru.itech.sno_api.service.implementation.toDTO
 import java.util.*
+
 
 @Schema(description = "Лекция")
 data class LectureDTO(
-    @Schema(description = "Идентификатор лекции", example = "1")
+    @Schema(description = "Идентификатор лекции")
     val lectureId: Long,
-
-    @Schema(description = "Идентификатор курса", example = "1", nullable = true)
-    val courseId: Long?, // Add this field
-
-    @Schema(description = "Лектор", nullable = true)
-    val lecturer: UserDTO?,
-
-    @Schema(description = "Название", example = "Введение в информатику")
+    @Schema(description = "Лектор")
+    val lecturer: UserInfoDTO,
+    @Schema(description = "Название")
     val title: String,
-
-    @Schema(description = "Описание", example = "Описание курса информатики.")
+    @Schema(description = "Описание")
     val description: String,
-
-    @Schema(description = "Дата", nullable = true, type = "string", format = "date-time", example = "2022-12-01T15:00:00Z")
-    val date: Date?,
-
-    @Schema(description = "Конспект", nullable = true)
-    val summary: SummaryDTO?,
-
-    @Schema(description = "Форум", nullable = true)
-    val forum: ForumDTO?,
-
-    @Schema(description = "Файл", nullable = true)
-    val file: FilesDTO?
+    @Schema(description = "Дата")
+    val date: Date,
+    @Schema(description = "Конспект")
+    val summary: SummaryDTO,
+    @Schema(description = "Форум")
+    val forum: ForumDTO,
+    val file: FilesDTO
 )
+fun LectureEntity.toDTO(): LectureDTO {
+    return LectureDTO(
+        lectureId = lectureId,
+        lecturer = lecturer.toDTO(),
+        title = title,
+        description = description,
+        date = date!!,
+        summary = summary.toDTO(),
+        forum = forum.toDTO(),
+        file = file.toDTO()
 
-@Transactional
-fun LectureDTO.toEntity(courseRepository: CourseRepository): LectureEntity {
-    val lecture = LectureEntity().apply {
-        lectureId = this@toEntity.lectureId
-        title = this@toEntity.title
-        description = this@toEntity.description
-        date = this@toEntity.date
-        course = this@toEntity.courseId?.let { courseRepository.findById(it).orElse(null) }
-        lecturer = this@toEntity.lecturer?.toEntity(courseRepository) // Adjust if necessary
-        summary = this@toEntity.summary?.toEntity()
-        forum = this@toEntity.forum?.toEntity()
-        file = this@toEntity.file?.toEntity()
-    }
-    return lecture
+    )
 }
+
