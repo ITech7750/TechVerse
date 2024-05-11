a5daef11b9a7902854273522809cbb1d2e88a3bb
Ivan <idfedorov09@gmail.com>
2024-05-11 18:15:16 +0300

    Frontend host config for cors

--- src/main/kotlin/ru/itech/sno_api/core/config/WebSecurityConfig.kt
+++ src/main/kotlin/ru/itech/sno_api/core/config/WebSecurityConfig.kt
@@ -1,7 +1,9 @@
 package ru.itech.sno_api.core.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -11,15 +13,21 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.stereotype.Component
 import ru.itech.sno_api.core.AuthEntryPoint
 import ru.itech.sno_api.core.AuthTokenFilter
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import java.util.Arrays
 
 @EnableWebSecurity
 @Configuration
+@Component
 open class WebSecurityConfig {
+
+    @Value("\${cors.frontend.host}")
+    private lateinit var frontendHost: String
 
     @Bean
     open fun provideAuthEntryPoint(): AuthenticationEntryPoint = AuthEntryPoint()
@@ -37,39 +45,49 @@ open class WebSecurityConfig {
         return BCryptPasswordEncoder()
     }
 
-    // Настройка CORS
+
     @Bean
     open fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("http://localhost:3000") // Для разработки братьям меньшим - фронтендерам
-        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
-        configuration.allowedHeaders = listOf("*") // Разрешаем все заголовки
-        configuration.allowCredentials = true
-        configuration.maxAge = 3600L
+        configuration.apply {
+            addAllowedOrigin(frontendHost)
+
+            HttpMethod.values().forEach {
+                addAllowedMethod(it)
+            }
+
+            addAllowedHeader("*")
+
+            allowCredentials = true
+            maxAge = 3600L
+        }
 
         val source = UrlBasedCorsConfigurationSource()
         source.registerCorsConfiguration("/**", configuration)
         return source
     }
 
+
     @Bean
     open fun filterChain(http: HttpSecurity): SecurityFilterChain {
         return http
-            .cors { cors -> cors.configurationSource(corsConfigurationSource()) } // Включаем CORS
-            .csrf { it.disable() } // Отключаем CSRF
+            .cors { cors ->
+                cors.configurationSource(corsConfigurationSource())
+            }
+            .csrf { it.disable() }
             .authorizeHttpRequests { authorize ->
                 authorize
                     .requestMatchers(
                         "/api/auth/login",
                         "/api/auth/register",
-                        "/api/**", // Разрешаем запросы к API
+                        "/api/**",
                         "/swagger-ui/**",
                         "/v3/api-docs/**",
                         "/swagger-ui.html",
                         "/favicon.ico",
                         "/webjars/**"
                     ).permitAll()
-                    .anyRequest().authenticated() // Остальные запросы требуют авторизации
+                    .anyRequest().authenticated()
             }
             .exceptionHandling { exception ->
                 exception.authenticationEntryPoint(provideAuthEntryPoint())
