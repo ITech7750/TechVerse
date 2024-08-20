5356e8bdc45ebb4f2640ce543299fa04690763d6
Игорь <tigor7750@gmail.com>
2024-08-20 10:44:53 +0300

    with filters and sorting

--- src/main/kotlin/ru/itech/sno_api/repository/LectureRepository.kt
+++ src/main/kotlin/ru/itech/sno_api/repository/LectureRepository.kt
@@ -1,13 +1,14 @@
 package ru.itech.sno_api.repository
 
 import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import ru.itech.sno_api.entity.LectureEntity
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 
 @Repository
-interface LectureRepository : CrudRepository<LectureEntity, Long>, JpaSpecificationExecutor<LectureEntity> {
+interface LectureRepository : JpaRepository<LectureEntity, Long>, JpaSpecificationExecutor<LectureEntity> {
     fun findByTitle(title: String): List<LectureEntity>
     fun findByOrderByLectureId(pageable: Pageable): List<LectureEntity>
     fun findByLecturerUserId(lecturerId: Long): List<LectureEntity>
